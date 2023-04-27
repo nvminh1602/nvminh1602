e9a3a2a42757312a8e08dcd6d5d1d9e034c991ac
nvminh1602 <nvminh1602@gmail.com>
2023-04-28 01:57:18 +0700

    upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+setTimeout(function() {
+    alert("Say hi, Tui là Mynh, Website đang trong quá trình phát triển, TKS For Coming! >.<");
+}, 3000);
+  
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
